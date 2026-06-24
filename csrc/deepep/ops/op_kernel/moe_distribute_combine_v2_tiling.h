ddc4252884c4f04738c2fa2f8971c5405a7024e4
Wenhao Ge <gewenhao2@h-partners.com>
2026-06-24 16:10:58 +0800

    deleted unnecessary code

--- csrc/deepep/ops/op_kernel/moe_distribute_combine_v2_tiling.h
+++ csrc/deepep/ops/op_kernel/moe_distribute_combine_v2_tiling.h
@@ -36,7 +36,6 @@ struct MoeDistributeCombineV2TilingData {
     Mc2InitTiling mc2InitTiling;
     Mc2CcTiling mc2CcTiling1;
     Mc2CcTiling mc2CcTiling2;
-    Mc2CcTiling mc2CcTiling;
     MoeDistributeCombineV2Info moeDistributeCombineV2Info;
 };
 
