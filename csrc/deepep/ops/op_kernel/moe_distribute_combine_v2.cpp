dcef0203ec036f8ba46594cf4475e98bae6bdc54
Wenhao Ge <gewenhao2@h-partners.com>
2026-06-25 09:36:27 +0800

    added one line to parse tilingData from tilingGM

--- csrc/deepep/ops/op_kernel/moe_distribute_combine_v2.cpp
+++ csrc/deepep/ops/op_kernel/moe_distribute_combine_v2.cpp
@@ -80,6 +80,7 @@ extern "C" __global__ __aicore__ void moe_distribute_combine_v2(
                 &tilingData);
         op.Process();
     } else if (TILING_KEY_IS(32000)) {
+        GET_TILING_DATA_WITH_STRUCT(MoeDistributeCombineV2TilingData, tilingData, tilingGM);
         MoeDistributeCombineA2Impl::MoeDistributeCombineV2Layered<DTYPE_EXPAND_X, int32_t, DTYPE_EXPAND_X> op;
         op.Init(expandX, expertIds, assistInfoForCombine, epSendCount, scales, XOut, workspaceGM, &pipe, tilingGM,
             tilingData);
