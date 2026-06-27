5d9d35fa41238fa0b99a9420a4024b54141d4635
Wenhao Ge <gewenhao2@h-partners.com>
2026-06-27 13:38:27 +0800

    made code changes

--- csrc/deepep/deep_ep.cpp
+++ csrc/deepep/deep_ep.cpp
@@ -15,6 +15,7 @@ constexpr uint32_t DYNAMIC_SCALES = 2;
 constexpr uint32_t MXFP8_SCALES = 3;
 constexpr uint32_t MXFP4_SCALES = 4;
 constexpr int LOCAL_RANK_SIZE = 8;
+constexpr int A3_LOCAL_RANK_SIZE = 16;
 constexpr int MAX_BATCH_SIZE = 4096;
 constexpr int EXPERT_DATA_SIZE = 1 + MAX_BATCH_SIZE;  // 4097
 constexpr int A3_MAX_HCCS_PEERS = 384;
@@ -855,6 +856,7 @@ Buffer::low_latency_dispatch(const at::Tensor &x, const at::Tensor &topk_idx,
     int64_t expert_shard_type = 0;
     int outType = get_value_from_env("MOE_EXPERT_TOKEN_NUMS_TYPE", 1);
     int isCcu = get_value_from_env("MOE_ENABLE_CCU", 0);
+    int isA3Layout = get_value_from_env("MOE_ENABLE_A3_LAYOUT", 0);
     char *comm_alg;
     int64_t expert_token_nums_type = outType;
 
@@ -879,12 +881,18 @@ Buffer::low_latency_dispatch(const at::Tensor &x, const at::Tensor &topk_idx,
             int64_t recv_count_tensor_size = num_experts + 2 * global_bs * num_topk * server_num;
             ep_recv_count = at::empty({recv_count_tensor_size}, at::dtype(at::kInt).device(device));
         }
+    } else if (isA3Layout == 1) {
+        server_num = num_ranks / A3_LOCAL_RANK_SIZE;
+        int64_t recv_count_tensor_size = num_experts + 2 * global_bs * num_topk * server_num;
+        ep_recv_count = at::empty({recv_count_tensor_size}, at::dtype(at::kInt).device(device));
     }
 
     if (soc_version == op::SocVersion::ASCEND910B) {
         comm_alg = "fullmesh";
     } else if (isCcu == 1) {
         comm_alg = "ccu";
+    } else if (isA3Layout == 1) {
+        comm_alg = "hierarchy";
     } else {
         comm_alg = "fullmesh_v1";
     }
@@ -911,7 +919,7 @@ Buffer::low_latency_dispatch(const at::Tensor &x, const at::Tensor &topk_idx,
                  quant_mode,
                  global_bs,               // global_bs
                  expert_token_nums_type,  // expert_token_nums_type
-                 "hierarchy",
+                 comm_alg,
                  packed_recv_x,         // expandXOut
                  packed_recv_x_scales,  // dynamicScalesOut
                  expandIdx,             // assistInfoForCombineOut
@@ -994,7 +1002,7 @@ std::tuple<at::Tensor, std::optional<EventHandle>, std::optional<std::function<v
                  tp_send_counts, x_active_mask, activation_scale, weight_scale, group_list, expand_scales,
                  shared_expert_x, hcom_ep_name, num_ranks, rank, num_experts, hcom_tp_name, tp_world_size, tp_rankId,
                  expert_shared_type, shared_expert_num, shared_expert_rank_num, global_bs, out_dtype, comm_quant_mode,
-                 group_list_type, "hierarchy", combined_x, combine_send_cost_stats_out);
+                 group_list_type, comm_alg, combined_x, combine_send_cost_stats_out);
 
     return {combined_x, event, std::function<void()>([] {})};
 }
